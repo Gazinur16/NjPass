cbf890c1d6872110eda2b9a0fad7f660d956196d
Njmin <gazinurnurtdinov35@gmail.com>
2023-09-30 03:33:12 +0300

    commit4

---
+++ bot/example_config.py
@@ -0,0 +1,8 @@
+PROD_MODE = False
+
+if PROD_MODE:
+    TG_BOT_TOKEN = 'YOU BOT TOKEN'
+    DB_PATH = 'database.db'
+else:
+    TG_BOT_TOKEN = 'YOU BOT TOKEN'
+    DB_PATH = 'example_database.db'
--- bot/misc.py
+++ bot/misc.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 from aiogram.utils.executor import Executor
-from config import TG_BOT_TOKEN
+from config import TG_BOT_TOKEN, DB_PATH
 from db import Database
 
 bot = Bot(token=TG_BOT_TOKEN, parse_mode=ParseMode.HTML)
@@ -13,4 +13,4 @@ dp = Dispatcher(bot=bot, storage=bot_storage)
 executor_ = Executor(dispatcher=dp, skip_updates=True)
 
 logging.basicConfig(level=logging.INFO)
-db = Database('database.db')
+db = Database(DB_PATH)
